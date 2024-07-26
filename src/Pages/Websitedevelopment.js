b07382735b82fc9bb44cb5f8b5d5bee5fdfca427
Murad Raza <31819169+muradraza@users.noreply.github.com>
2024-07-27 02:43:21 +0500

    web page

--- src/Pages/Websitedevelopment.js
+++ src/Pages/Websitedevelopment.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Pagebanner from "../Components/Pagebanner";
+import Counter from "../Components/Counter";
 
 function Websitedevelopment() {
   return (
@@ -160,6 +161,206 @@ function Websitedevelopment() {
           </div>
         </div>
       </div>
+
+      <div
+        className="aproch"
+        style={{
+          marginTop: "15px",
+        }}
+      >
+        <div className="approchdetails">
+          <h3>Create a Unique Website that Sell Your Business Online!</h3>
+          <p>
+            We have a combination of both professional design and development
+            expertise that gives us the capabilities of building some complex
+            and custom web solutions for you.
+          </p>
+          <br />
+          <br />
+
+          <div
+            style={{
+              display: "flex",
+              alignContent: "center",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "20%",
+              }}
+            >
+              <p
+                style={{
+                  backgroundColor: "#B0CBFF",
+                  padding: "20px",
+                  color: "#1E68F8",
+                  borderRadius: "50%",
+                }}
+              >
+                01
+              </p>
+            </div>
+            <div
+              style={{
+                width: "80%",
+              }}
+            >
+              <div>
+                <h4>Wireframe & Design</h4>
+              </div>
+              <div>
+                <p>
+                  Our design craftsman lay down some of the best visual concepts
+                  and strategy to make sure your brand stands out in
+                  competition, resulting in better ROI.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignContent: "center",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "20%",
+              }}
+            >
+              <p
+                style={{
+                  backgroundColor: "#B0CBFF",
+                  color: "#1E68F8",
+                  padding: "20px",
+                  borderRadius: "50%",
+                }}
+              >
+                02
+              </p>
+            </div>
+            <div
+              style={{
+                width: "80%",
+              }}
+            >
+              <div>
+                <h4>Good Web Design & Development</h4>
+              </div>
+              <div>
+                <p>
+                  After our design team is finished with the visuals, the
+                  development team comes into action and makes sure that each
+                  and every functionality blended in perfectly and goes with the
+                  design.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignContent: "center",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "20%",
+              }}
+            >
+              <p
+                style={{
+                  backgroundColor: "#B0CBFF",
+                  color: "#1E68F8",
+                  padding: "20px",
+                  borderRadius: "50%",
+                }}
+              >
+                03
+              </p>
+            </div>
+            <div
+              style={{
+                width: "80%",
+              }}
+            >
+              <div>
+                <h4>Launch</h4>
+              </div>
+              <div>
+                <p>
+                  After your business website finally looks and feels just the
+                  way you wanted it, we make it visible to all of your targeted
+                  viewers online.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="aprochimg">
+          <img src="\show1.png" alt="..." />
+        </div>
+      </div>
+
+      {/* <!-- Gallery --> */}
+      <div
+        class="row"
+        style={{ width: "80%", margin: "auto", paddingTop: "50px" }}
+      >
+        <div class="col-lg-4 col-md-12 mb-4 mb-lg-0 sepic">
+          <img
+            src="/seopic/11.png"
+            class="w-100 shadow-1-strong rounded mb-4"
+            alt="Boat on Calm Water"
+          />
+
+          <img
+            src="/seopic/12.png"
+            class="w-100 shadow-1-strong rounded mb-4"
+            alt="Boat on Calm Water"
+          />
+        </div>
+
+        <div class="col-lg-4 mb-4 mb-lg-0 sepic">
+          <img
+            src="/seopic/14.png"
+            class="w-100 shadow-1-strong rounded mb-4"
+            alt="Mountains in the Clouds"
+          />
+
+          <img
+            src="/seopic/15.png"
+            class="w-100 shadow-1-strong rounded mb-4"
+            alt="Boat on Calm Water"
+          />
+        </div>
+
+        <div class="col-lg-4 mb-4 mb-lg-0 sepic">
+          <img
+            src="/seopic/15.png"
+            class="w-100 shadow-1-strong rounded mb-4"
+            alt="Boat on Calm Water"
+          />
+
+          <img
+            src="/seopic/13.png"
+            class="w-100 shadow-1-strong rounded mb-4"
+            alt="Wintry Mountain Landscape"
+          />
+        </div>
+      </div>
+      {/* <!-- Gallery --> */}
+
+      <Counter />
     </>
   );
 }
