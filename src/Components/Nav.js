4f6e5bb5f7ccf4032fef95868adb3e2c2c2686bc
Murad Raza <31819169+muradraza@users.noreply.github.com>
2024-07-29 22:35:48 +0500

    Navbar updated

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,103 +1,110 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Link } from "react-router-dom";
 
 function Nav() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-          <Link class="navbar-brand" to="/">
-            <img src="/tcubelogo.png" alt="" width="200px" height="70px" />
-          </Link>
+      <div className="cont">
+        <nav className="container navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container-fluid">
+            <Link class="navbar-brand" to="/">
+              <img src="/tcubelogo.png" alt="" width="200px" height="70px" />
+            </Link>
 
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" to="/">
-                  Home
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/about">
-                  About
-                </Link>
-              </li>
-              <li class="nav-item dropdown">
-                <Link
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Services
-                </Link>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <Link class="dropdown-item" to={"/shopify"}>
-                      Shopify Development
-                    </Link>
-                  </li>
-                  <li>
-                    <Link class="dropdown-item" to={"/seo"}>
-                      Seo
-                    </Link>
-                  </li>
-                  <li>
-                    <Link class="dropdown-item" to={"/websitedevelopment"}>
-                      Website Development
-                    </Link>
-                  </li>
-                  <li>
-                    <Link class="dropdown-item" to={"/mobile-app"}>
-                      Mobile App Development
-                    </Link>
-                  </li>
-                  <li>
-                    <Link class="dropdown-item" to={"/smm"}>
-                      Social Media Marketing
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/showcase">
-                  Showcase
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link" to="/testimonials">
-                  Testimonials
-                </Link>
-              </li>
-
-              <li class="nav-item">
-                <Link class="nav-link" to="/contact">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-
-            <button type="button" class="btn btn-primary">
-              <a className="tel" href="tel: 03322082393">
-                +92 332 2082 393
-              </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
             </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <Link class="nav-link active" aria-current="page" to="/">
+                    Home
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" to="/about">
+                    About
+                  </Link>
+                </li>
+                <li class="nav-item dropdown">
+                  <Link
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Services
+                  </Link>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li>
+                      <Link class="dropdown-item" to={"/shopify"}>
+                        Shopify Development
+                      </Link>
+                    </li>
+                    <li>
+                      <Link class="dropdown-item" to={"/seo"}>
+                        Seo
+                      </Link>
+                    </li>
+                    <li>
+                      <Link class="dropdown-item" to={"/websitedevelopment"}>
+                        Website Development
+                      </Link>
+                    </li>
+                    <li>
+                      <Link class="dropdown-item" to={"/mobile-app"}>
+                        Mobile App Development
+                      </Link>
+                    </li>
+                    <li>
+                      <Link class="dropdown-item" to={"/smm"}>
+                        Social Media Marketing
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" to="/showcase">
+                    Showcase
+                  </Link>
+                </li>
+                <li class="nav-item">
+                  <Link class="nav-link" to="/testimonials">
+                    Testimonials
+                  </Link>
+                </li>
+
+                <li class="nav-item">
+                  <Link class="nav-link" to="/contact">
+                    Contact
+                  </Link>
+                </li>
+              </ul>
+
+              <button
+                type="button"
+                class="btn btn-primary rounded-pill navbtn py-2 px-4"
+              >
+                <Link className="tel" to="tel: 7277774610)">
+                  <FontAwesomeIcon icon="fa-solid fa-phone" />
+                  (727) 777 4610
+                </Link>
+              </button>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </>
   );
 }
--- src/Pages/Showcase.js
+++ src/Pages/Showcase.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Pagebanner from "../Components/Pagebanner";
 import Showcases from "../Components/Showcases";
+import { Helmet } from "react-helmet";
 
 function Showcase() {
   return (
     <>
+      <Helmet>
+        <title>Showcase Your Innovation with TeeCube Solutions Ltd</title>
+        <meta
+          name="description"
+          content="Transform your vision into reality with TeeCube Solutions Ltd. Explore our showcase to see cutting-edge projects and happy clients."
+        />
+      </Helmet>
       <Pagebanner name={"Show Case"} />
       <Showcases />
     </>
